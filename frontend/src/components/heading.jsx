605d1dbef5f1b95b63120cad2fb4e166b5ce8657
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-06 21:20:48 +0530

    stopped unnecessary rerender

--- frontend/src/components/heading.jsx
+++ frontend/src/components/heading.jsx
@@ -3,7 +3,6 @@ const Heading = React.memo(function ({ label }) {
     return (
         <div className="font-bold pt-3 text-4xl">
             {label}
-            {console.log("555")}
         </div>
     )
 })
