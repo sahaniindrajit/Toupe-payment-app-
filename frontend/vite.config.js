38955c3ef1dbfbcc41de03cfe79756f37c13a628
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-18 14:27:00 +0530

    production update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/v1': 'http://localhost:3500/api'
+      '/v1': 'https://toupe-payment-app-backend.onrender.com/api'
     }
   },
   plugins: [react()],
