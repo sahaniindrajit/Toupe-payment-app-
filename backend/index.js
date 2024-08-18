8907f1386423ed3ffe10641610f12013d49e0828
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-18 21:05:37 +0530

    cors configer

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: 'https://toupe-payment-app-frontend.onrender.com',
+    origin: 'https://toupe-payment-app.netlify.app/',
     credentials: true
 
 }));
